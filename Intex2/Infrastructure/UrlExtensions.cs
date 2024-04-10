8338ba545ae27dab90d3f91c2763807728d9469c
Chrysanthe Belgique Johnston <belgiquc@byu.edu>
2024-04-10 14:14:06 -0600

    updated cart

--- Intex2/Infrastructure/UrlExtensions.cs
+++ Intex2/Infrastructure/UrlExtensions.cs
@@ -1,8 +1,8 @@
-﻿//namespace Intex2.Infrastructure
-//{
-//    public class UrlExtensions
-//    {
-//        public static string PathAndQuery(this HttpRequest request) =>
-//            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
-//    }
-//}
+﻿namespace Intex2.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
+    }
+}
