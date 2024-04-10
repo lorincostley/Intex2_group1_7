c6bf9a12a3c8ab80a0445c875a624fc9e38b1301
lorincostley <120672798+lorincostley@users.noreply.github.com>
2024-04-10 14:45:20 -0600

    controllereedf

---
+++ Intex2/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intex2.Models;
+
+public partial class Customer
+{
+    public short CustomerId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    public string? CountryOfResidence { get; set; }
+
+    public string? Gender { get; set; }
+
+    public double? Age { get; set; }
+
+    public string? CustEmail { get; set; }
+}
