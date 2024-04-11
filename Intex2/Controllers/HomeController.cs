0ae9cc8a094aa04873fd92c0cfef9f291b7319f8
lorincostley <120672798+lorincostley@users.noreply.github.com>
2024-04-11 10:02:33 -0600

    product list stuff

--- Intex2/Controllers/HomeController.cs
+++ Intex2/Controllers/HomeController.cs
@@ -34,9 +34,19 @@ namespace Intex2.Controllers
             return View();
         }
 
-        public IActionResult ProductSummary(int productId)
+        public ActionResult ProductDetails(int ProductId, string Name, string ImgLink, int Price, string Description)
         {
-            return View(ProductDetails);
+
+            var product = new Product
+            {
+                ProductId = ProductId,
+                Name = Name,
+                ImgLink = ImgLink,
+                Price = Price,
+                Description = Description
+            };
+
+            return View(product);
         }
 
         public IActionResult ProductList(int pageNum, string? productType)
@@ -129,11 +139,6 @@ namespace Intex2.Controllers
         public IActionResult Login(string var)
         {
             return RedirectToAction("SignUp");
-        }
-
-        public IActionResult ProductDetails()
-        {
-            return View();
         }
         public IActionResult SignUp()
         {
